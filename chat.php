48082d960d4a2f14d49011740e3fc7051363ee35
Ubuntu <ubuntu@ip-10-251-64-80.us-west-2.compute.internal>
2013-10-17 14:18:40 +0000

    Return and parse JSON.

--- chat.php
+++ chat.php
@@ -4,11 +4,15 @@ require "db_info.php";
 
 $link = mysql_connect($location, $user, $pass) or die(mysql_error());
 mysql_select_db($db) or die (mysql_error());
-$sql = "SELECT * FROM chat";
+$update_token = $_GET["update_token"];
+$sql = "SELECT * FROM chat where update_token > " . $update_token;
 $result = mysql_query($sql) or die(mysql_error());
 
+$data = array();
 while ($row = mysql_fetch_array($result))
 {
-  echo $row[0] . " and time is " . time();
+  array_push($data, array('user_id' => $row['user_id'], 'update_token' => (int)$row['update_token'], 'text' => $row['text'], 'time' => time()));
 }
+
+echo json_encode($data);
 ?>
